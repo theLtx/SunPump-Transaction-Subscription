2b29074cccf04f803fee4efc4af7aa41cf4ce49e
theLtx <103037710+theLtx@users.noreply.github.com>
2024-09-11 20:51:12 +0200

    Update connection

---
+++ connection.ts
@@ -0,0 +1,16 @@
+import zmq from "zeromq";
+
+
+let sock = zmq.socket("sub");
+sock.connect("tcp://xxx:5555");
+sock.subscribe("contractLog");
+console.log("Subscriber connected to port 5555");
+
+sock.on("message", function(topic, message) {
+    console.log(
+      "received a message related to:",
+      Buffer.from(topic).toString(),
+      ", containing message:",
+      Buffer.from(message).toString()
+    );
+  });
