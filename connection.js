65075cbf735ac8f6d8b3648fadc50b51e64094a9
theLtx <103037710+theLtx@users.noreply.github.com>
2024-09-11 20:36:44 +0200

    Add Connection script

---
+++ connection.js
@@ -0,0 +1,16 @@
+// subscriber.js
+var zmq = require("zeromq"),
+var sock = zmq.socket("sub");
+
+sock.connect("tcp://127.0.0.1:5555");
+sock.subscribe("block");
+console.log("Subscriber connected to port 5555");
+
+sock.on("message", function(topic, message) {
+  console.log(
+    "received a message related to:",
+    Buffer.from(topic).toString(),
+    ", containing message:",
+    Buffer.from(message).toString()
+  );
+});
